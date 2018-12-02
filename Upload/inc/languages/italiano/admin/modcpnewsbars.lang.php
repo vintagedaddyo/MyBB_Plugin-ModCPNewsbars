9154ef50aef494b305bdae75a910d19ebc821fe4
vintagedaddyo <vintagedaddyo@gmail.com>
2018-12-01 18:45:59 -0800

    Update modcpnewsbars.lang.php

--- Upload/inc/languages/italiano/admin/modcpnewsbars.lang.php
+++ Upload/inc/languages/italiano/admin/modcpnewsbars.lang.php
@@ -16,9 +16,9 @@
 
 $l['modcpnewsbars_name'] = 'ModCP Newsbars';
 $l['modcpnewsbars_desc'] = 'Aggiunge newsbar alla pagina modcp del tuo forum.';
-$l['modcpnewsbars_web'] = 'http://community.mybb.com/mod-6029.html';
+$l['modcpnewsbars_web'] = 'http://community.mybb.com/user-6029.html';
 $l['modcpnewsbars_auth'] = 'Vintagedaddyo';
-$l['modcpnewsbars_authsite'] = 'http://community.mybb.com/mod-6029.html';
+$l['modcpnewsbars_authsite'] = 'http://community.mybb.com/user-6029.html';
 $l['modcpnewsbars_ver'] = '1.1';
 $l['modcpnewsbars_compat'] = '18*';
 
@@ -30,7 +30,7 @@ $l['modcpnewsbars_description_setting_group'] = 'Impostazioni per il plug-in Mod
 // Impostazione 1
 
 $l['modcpnewsbars_title_setting_1'] = 'Vuoi abilitare il plugin ModCnewsbars su ModCP?';
-$l['modcpnewsbars_description_setting_1'] = 'Se si imposta questa opzione su yes, questo plugin sarà attivo sul proprio indice.';
+$l['modcpnewsbars_description_setting_1'] = 'Se si imposta questa opzione su yes, questo plugin sara attivo sul proprio indice.';
 
 // Impostazione 2
 
@@ -40,7 +40,7 @@ $l['modcpnewsbars_description_setting_2'] = 'Inserisci il testo che vuoi visuali
 // Impostazione 3
 
 $l['modcpnewsbars_title_setting_3'] = 'Vuoi abilitare l avviso delle barattive di ModCP?';
-$l['modcpnewsbars_description_setting_3'] = 'Se si imposta questa opzione su yes, Alter sarà attivo su modcp.';
+$l['modcpnewsbars_description_setting_3'] = 'Se si imposta questa opzione su yes, Alter sara attivo su modcp.';
 
 // Impostazione 4
 
@@ -50,7 +50,7 @@ $l['modcpnewsbars_description_setting_4'] = 'Inserisci il testo che vuoi visuali
 // Impostazione 5
 
 $l['modcpnewsbars_title_setting_5'] = 'Vuoi abilitare l avviso di notifica 1 di ModCP?';
-$l['modcpnewsbars_description_setting_5'] = 'Se si imposta questa opzione su yes, l avviso 1 sarà attivo su modcp.';
+$l['modcpnewsbars_description_setting_5'] = 'Se si imposta questa opzione su yes, l avviso 1 sara attivo su modcp.';
 
 // Impostazione 6
 
@@ -60,7 +60,7 @@ $l['modcpnewsbars_description_setting_6'] = 'Inserisci il testo che vuoi visuali
 // Impostazione 7
 
 $l['modcpnewsbars_title_setting_7'] = 'Vuoi abilitare l avviso 2 di ModCarobar?';
-$l['modcpnewsbars_description_setting_7'] = 'Se si imposta questa opzione su yes, l avviso 2 sarà attivo su modcp.';
+$l['modcpnewsbars_description_setting_7'] = 'Se si imposta questa opzione su yes, l avviso 2 sara attivo su modcp.';
 
 // Impostazione 8
 
